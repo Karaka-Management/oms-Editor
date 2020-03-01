9747c036aba85daaff7b2c058237ae4be05b93df
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-01 15:00:33 +0100

    draft tags and portlets

--- Theme/Backend/editor-list.tpl.php
+++ Theme/Backend/editor-list.tpl.php
@@ -21,29 +21,26 @@ $docs = $this->getData('docs');
 echo $this->getData('nav')->render(); ?>
 <div class="row">
     <div class="col-xs-12">
-        <div class="box wf-100">
+        <div class="portlet">
+            <div class="portlet-head"><?= $this->getHtml('Documents') ?><i class="fa fa-download floatRight download btn"></i></div>
             <table class="default">
-                <caption><?= $this->getHtml('Documents') ?><i class="fa fa-download floatRight download btn"></i></caption>
-                <thead>
-                <tr>
-                    <td class="wf-100"><?= $this->getHtml('Title') ?>
-                    <td><?= $this->getHtml('Creator') ?>
-                    <td><?= $this->getHtml('Created') ?>
-                <tfoot>
-                <tr>
-                    <td colspan="3">
-                <tbody>
-                <?php $count = 0; foreach ($docs as $key => $value) : ++$count;
-                $url = \phpOMS\Uri\UriFactory::build('{/prefix}editor/single?{?}&id=' . $value->getId()); ?>
-                    <tr data-href="<?= $url; ?>">
-                        <td data-label="<?= $this->getHtml('Title') ?>"><a href="<?= $url; ?>"><?= $this->printHtml($value->getTitle()); ?></a>
-                        <td data-label="<?= $this->getHtml('Creator') ?>"><a href="<?= $url; ?>"><?= $this->printHtml($value->getCreatedBy()->getName1()); ?></a>
-                        <td data-label="<?= $this->getHtml('Created') ?>"><a href="<?= $url; ?>"><?= $this->printHtml($value->getCreatedAt()->format('Y-m-d H:i:s')); ?></a>
-                <?php endforeach; ?>
-                <?php if ($count === 0) : ?>
-                    <tr><td colspan="3" class="empty"><?= $this->getHtml('Empty', '0', '0'); ?>
-                <?php endif; ?>
-            </table>
-        </div>
+            <thead>
+            <tr>
+                <td class="wf-100"><?= $this->getHtml('Title') ?>
+                <td><?= $this->getHtml('Creator') ?>
+                <td><?= $this->getHtml('Created') ?>
+            <tbody>
+            <?php $count = 0; foreach ($docs as $key => $value) : ++$count;
+            $url = \phpOMS\Uri\UriFactory::build('{/prefix}editor/single?{?}&id=' . $value->getId()); ?>
+                <tr data-href="<?= $url; ?>">
+                    <td data-label="<?= $this->getHtml('Title') ?>"><a href="<?= $url; ?>"><?= $this->printHtml($value->getTitle()); ?></a>
+                    <td data-label="<?= $this->getHtml('Creator') ?>"><a href="<?= $url; ?>"><?= $this->printHtml($value->getCreatedBy()->getName1()); ?></a>
+                    <td data-label="<?= $this->getHtml('Created') ?>"><a href="<?= $url; ?>"><?= $this->printHtml($value->getCreatedAt()->format('Y-m-d H:i:s')); ?></a>
+            <?php endforeach; ?>
+            <?php if ($count === 0) : ?>
+                <tr><td colspan="3" class="empty"><?= $this->getHtml('Empty', '0', '0'); ?>
+            <?php endif; ?>
+        </table>
+        <div class="portlet-foot"></div>
     </div>
 </div>
