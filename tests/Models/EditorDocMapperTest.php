e3c725f16d37b7ff2a1025566e2106fe7e1441df
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-14 14:14:22 +0100

    add tests

---
+++ tests/Models/EditorDocMapperTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package    tests
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\tests\Editor\Models;
+
+use Modules\Admin\Models\NullAccount;
+use Modules\Editor\Models\EditorDoc;
+use Modules\Editor\Models\EditorDocMapper;
+use phpOMS\Utils\RnG\Text;
+
+/**
+ * @internal
+ */
+class EditorDocMapperTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\Editor\Models\EditorDocMapper
+     * @group module
+     */
+    public function testCRUD() : void
+    {
+        $doc = new EditorDoc();
+
+        $doc->setCreatedBy(new NullAccount(1));
+        $doc->setTitle('Title');
+        $doc->setContent('Content');
+        $doc->setPath('/some/test/path');
+
+        $id = EditorDocMapper::create($doc);
+        self::assertGreaterThan(0, $doc->getId());
+        self::assertEquals($id, $doc->getId());
+
+        $docR = EditorDocMapper::get($doc->getId());
+        self::assertEquals($doc->getCreatedAt()->format('Y-m-d'), $docR->getCreatedAt()->format('Y-m-d'));
+        self::assertEquals($doc->getCreatedBy()->getId(), $docR->getCreatedBy()->getId());
+        self::assertEquals($doc->getContent(), $docR->getContent());
+        self::assertEquals($doc->getTitle(), $docR->getTitle());
+        self::assertEquals($doc->getPath(), $docR->getPath());
+    }
+
+    /**
+     * @group volume
+     * @group module
+     * @coversNothing
+     */
+    public function testVolume() : void
+    {
+        for ($i = 0; $i < 100; ++$i) {
+            $text = new Text();
+            $doc  = new EditorDoc();
+
+            // Test other
+
+            $doc->setCreatedBy(new NullAccount(\mt_rand(1, 1)));
+            $doc->setTitle($text->generateText(\mt_rand(3, 7)));
+            $doc->setContent($text->generateText(\mt_rand(20, 500)));
+            $doc->setPath('/some/test/path');
+
+            $id = EditorDocMapper::create($doc);
+        }
+    }
+}
---
+++ tests/Models/EditorDocTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package    tests
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\tests\Tasks\Models;
+
+use Modules\Admin\Models\NullAccount;
+use Modules\Editor\Models\EditorDoc;
+
+/**
+ * @internal
+ */
+class EditorDocTest extends \PHPUnit\Framework\TestCase
+{
+    public function testDefault() : void
+    {
+        $doc = new EditorDoc();
+
+        self::assertEquals(0, $doc->getId());
+        self::assertEquals(0, $doc->getCreatedBy()->getId());
+        self::assertEquals('', $doc->getTitle());
+        self::assertEquals('', $doc->getContent());
+        self::assertEquals('', $doc->getPlain());
+        self::assertEquals((new \DateTime('now'))->format('Y-m-d'), $doc->getCreatedAt()->format('Y-m-d'));
+    }
+
+    public function testSetGet() : void
+    {
+        $doc = new EditorDoc();
+
+        $doc->setCreatedBy(new NullAccount(1));
+        self::assertEquals(1, $doc->getCreatedBy()->getId());
+
+        $doc->setTitle('Title');
+        self::assertEquals('Title', $doc->getTitle());
+
+        $doc->setContent('Content');
+        self::assertEquals('Content', $doc->getContent());
+
+        $doc->setPlain('Plain');
+        self::assertEquals('Plain', $doc->getPlain());
+
+        $doc->setPath('/some/path');
+        self::assertEquals('/some/path', $doc->getPath());
+
+        $arr = [
+            'id' => 0,
+            'title' => $doc->getTitle(),
+            'plain' => $doc->getPlain(),
+            'content' => $doc->getContent(),
+            'createdAt' => $doc->getCreatedAt(),
+            'createdBy' => $doc->getCreatedBy(),
+        ];
+        self::assertEquals($arr, $doc->toArray());
+        self::assertEquals($arr, $doc->jsonSerialize());
+        self::assertEquals(\json_encode($arr), $doc->__toString());
+    }
+}
