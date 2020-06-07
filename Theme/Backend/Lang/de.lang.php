2ced0c832523f886ba6c7240d12f7faf021285f3
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-06-07 22:22:23 +0200

    add ger language

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Editor
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Editor' => [
+    'Created'    => 'Erstellt',
+    'Creator'    => 'Ersteller',
+    'Documents'  => 'Dokumente',
+    'Editor'     => 'Editor',
+    'GroupUser'  => 'Gruppe/Account',
+    'Insert'     => 'Einfügen',
+    'Layout'     => 'Layout',
+    'Name'       => 'Name',
+    'Permission' => 'Rechte',
+    'Preview'    => 'Vorschau',
+    'Save'       => 'Speichern',
+    'Start'      => 'Start',
+    'Text'       => 'Text',
+    'Title'      => 'Titel',
+]];
