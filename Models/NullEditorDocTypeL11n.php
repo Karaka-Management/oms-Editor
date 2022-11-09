08e4fa6c0f7cec6a32d9622751cb366f72ce956f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-09 23:02:52 +0100

    reduce serialization of null models

--- Models/NullEditorDocTypeL11n.php
+++ Models/NullEditorDocTypeL11n.php
@@ -36,4 +36,12 @@ final class NullEditorDocTypeL11n extends EditorDocTypeL11n
         $this->id = $id;
         parent::__construct();
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
