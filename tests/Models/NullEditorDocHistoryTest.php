2afad600d353af35b5f288c2f72acacdba1aad01
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 23:44:00 +0000

    Remove getId() function call from null models

--- tests/Models/NullEditorDocHistoryTest.php
+++ tests/Models/NullEditorDocHistoryTest.php
@@ -37,7 +37,7 @@ final class NullEditorDocHistoryTest extends \PHPUnit\Framework\TestCase
     public function testId() : void
     {
         $null = new NullEditorDocHistory(2);
-        self::assertEquals(2, $null->getId());
+        self::assertEquals(2, $null->id);
     }
 
     /**
